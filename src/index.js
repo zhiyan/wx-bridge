3dcf7a68fbe2582a0bed6ce7286ebc91112c8b94
zhiyan <zhiyan1987@gmail.com>
2017-07-12 13:00:26 +0800

    dump

--- src/index.js
+++ src/index.js
@@ -37,10 +37,6 @@ export default function(config = window.__WX_AUTH__ || {}){
   script.onload = () => {
 		if(typeof wx !== 'undefined'){
 
-			if(dev){
-				wx.ready = cb => cb()
-			}
-
 			config.api = config.api || ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ']
 
 			// 默认值
@@ -51,7 +47,7 @@ export default function(config = window.__WX_AUTH__ || {}){
 
 			wx.config({
 					// 开启调试模式,调用的所有api的返回值会在客户端alert出来,若要查看传入的参数,可以在pc端打开,参数信息会通过log打出,仅在pc端时才会打印。
-	        debug: config.debug || (dev ? true : false), 
+	        debug: config.debug || false, 
 
 	        // 必填,公众号的唯一标识
 	        appId: config.appid || config.appId || '', 
